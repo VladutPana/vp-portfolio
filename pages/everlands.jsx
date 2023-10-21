771bb8f69e713dc56b1f7a96a6cc4a1281bdece4
Vlad Pana <vladpana02@gmail.com>
2023-10-21 18:33:26 +0300

    updated
    21/10/2023

--- pages/everlands.jsx
+++ pages/everlands.jsx
@@ -17,7 +17,7 @@ const everlands = () => {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Everlands</h2>
+          <h2 className='py-2'>Expandable Cards</h2>
           <h3>React JS / Tailwind / FramerMotion</h3>
         </div>
       </div>
@@ -27,17 +27,21 @@ const everlands = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p className='mt-6'>
-            I built this application in React JS and Next JS 13.
-            This app features unique design based on tailwind css and framer motion which is based on animations. 
-            Currently is just a design and animation project, without a database and/or concise algorithms behind it.
-            It features a multiverse-like theme, named everlands which i took inspiration from the game no-man s-sky where planets and new galaxies could be explored.
+          The Expandable Cards App is an interactive web application that showcases a collection of expandable cards built with React, Tailwind CSS, and Framer Motion. This application allows users to click on cards to expand or collapse them, revealing additional information about each card.
           </p>
           <a
-            href='https://github.com/'
+            href='https://github.com/VladutPana/vp-portfolio'
             target='_blank'
             rel='noreferrer'
           >
             <button type="button" className="py-2 px-8 mr-8 mt-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 shadow-[#5651e5] hover:scale-110 ease-in duration-300">Github</button>
+          </a>
+          <a
+            href='https://vladutpana.github.io/vp-expandable-cards/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button type="button" className="py-2 px-8 mr-8 mt-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 shadow-[#5651e5] hover:scale-110 ease-in duration-300">Live Demo</button>
           </a>
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
@@ -51,13 +55,7 @@ const everlands = () => {
                 <RiRadioButtonFill className='pr-1' /> Tailwind
               </p>
               <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Javascript
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> Framer Motion
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Next 13
               </p>
             </div>
           </div>
